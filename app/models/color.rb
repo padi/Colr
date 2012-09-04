b836ad3d0f3e9266da34e07efb5398558fefdfda
Marc Rendl Ignacio <boracay+marc@aelogica.com>
2012-09-04 16:59:25 +0800

    Added Color and Tag models
    - also add .gitignore and updated Rakefile

---
+++ app/models/color.rb
@@ -0,0 +1,33 @@
+class Color
+  PROPERTIES = [:timestamp, :hex, :id, :tags]
+  PROPERTIES.each { |prop|
+    attr_accessor prop
+  }
+
+  def initialize hash={}
+    hash.each { |key, value|
+      if PROPERTIES.member? key.to_sym
+        self.send key.to_s + "=", value
+      end
+    }
+  end
+
+  # Custom getter/setter methods for tags
+  def tags
+    @tags ||= []
+  end
+
+  def tags= tags
+    if tags.first.is_a? Hash
+      tags = tags.collect { |tag| Tag.new tag }
+    end
+
+    tags.each { |tag|
+      if not tag.is_a? Tag
+        raise "Wrong class for attempted tag #{tag.inspect}"
+      end
+    }
+
+    @tags = tags
+  end
+end
